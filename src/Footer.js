d7e47e7aac8c09c238295a103591380633da86eb
Sujata Radadiya <165806733+sujata-radadiya@users.noreply.github.com>
2024-05-31 17:56:34 +0530

    Update Footer.js
    update code

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <>
+   <div className='container'>
+      <div className='row'>
+        <div className='col-12 col-md-3'>
+         <h1 className='line1'>Funiro.</h1>
+         <p className='praa'>400 University Drive Suite 200 Coral Gables,
+FL 33134 USA</p>
+        </div>
+        <div className='col-12 col-md-3'>
+          <h2>Links</h2>
+          <NavLink to="/" className="nav__link">
+               Home
+             </NavLink>
+          <p>Shop</p>
+          <p>About</p>
+          <p>Home</p>
+
+        </div>
+        <div className='col-12 col-md-3'>
+          
+        </div>
+        <div className='col-12 col-md-3'>
+          
+        </div>
+      </div>
+    </div>
+    </>
+     
+    
   )
 }
 
